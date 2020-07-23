c4ce0b8f8ebf6f36c519d03d0db9f9773054027e
fcai254 <53529671+fcai254@users.noreply.github.com>
2020-07-23 13:06:55 -0400

    adding my name and icebreaker
    ahhhhhhh

--- Day1/src/main/java/frc/robot/IceBreaker.java
+++ Day1/src/main/java/frc/robot/IceBreaker.java
@@ -5,10 +5,12 @@ public class IceBreaker {
     // NICKNAMES
     // Michael Gorbunov - Michael
     // Siddharth Lohani - Sid
+    // Flora Cai - Flora
 
     // Embarassing moments
     // Michael Gorbunov - Once asked out someone who turned out to be lesbian.
     // Siddharth Lohani - In first grade I had to go to the ER because I put a rock in my ear and I couldn't get it out.
+    // Flora Cai - During my ninth grade english midterm, my stomach growling really loudly and my friend started laughing.
 
     public IceBreaker () {
 
