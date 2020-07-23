67d8e8fd63791aabbb904c2d067748f3abea4c98
SarthakJain2 <sarthakjain.2007@gmail.com>
2020-07-23 13:26:55 -0400

    Update IceBreaker.java

--- Day1/src/main/java/frc/robot/IceBreaker.java
+++ Day1/src/main/java/frc/robot/IceBreaker.java
@@ -6,11 +6,14 @@ public class IceBreaker {
     // Michael Gorbunov - Michael
     // Siddharth Lohani - Sid
     // Shaheer Saud - Shaheer
+    //Sarthak Jain - Sarthak
 
     // Embarassing moments
     // Michael Gorbunov - Once asked out someone who turned out to be lesbian.
     // Siddharth Lohani - In first grade I had to go to the ER because I put a rock in my ear and I couldn't get it out.
     // Shaheer Saud - I ate a lot of biryani (brown food) and threw up and fainted. Turns out I am allergic to rice.
+   //Sarthak Jain - I once tied the laces between my shoes and my teacher made me walk to the nurse to get my laces cut
+
     public IceBreaker () {
 
     }
