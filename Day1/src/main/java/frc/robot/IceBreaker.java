3c417ac0d9adbdff5ebb497dcd13f8556b63b525
Shaheer Saud <63514357+ssaud1@users.noreply.github.com>
2020-07-23 13:09:45 -0400

    Update IceBreaker.java

--- Day1/src/main/java/frc/robot/IceBreaker.java
+++ Day1/src/main/java/frc/robot/IceBreaker.java
@@ -9,7 +9,7 @@ public class IceBreaker {
     // Embarassing moments
     // Michael Gorbunov - Once asked out someone who turned out to be lesbian.
     // Siddharth Lohani - In first grade I had to go to the ER because I put a rock in my ear and I couldn't get it out.
-
+    // Shaheer Saud - I ate a lot of biryani (brown food) and threw up and fainted. Turns out I am allergic to rice.
     public IceBreaker () {
 
     }
