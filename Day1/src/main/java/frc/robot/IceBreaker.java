ca78fa408f436a73ddc5658a5f97b66d847e2e03
siddharthlohani05 <68655332+siddharthlohani05@users.noreply.github.com>
2020-07-23 12:42:35 -0400

    Update IceBreaker.java

--- Day1/src/main/java/frc/robot/IceBreaker.java
+++ Day1/src/main/java/frc/robot/IceBreaker.java
@@ -4,9 +4,11 @@ public class IceBreaker {
 
     // NICKNAMES
     // Michael Gorbunov - Michael
+    // Siddharth Lohani - Sid
 
     // Embarassing moments
     // Michael Gorbunov - Once asked out someone who turned out to be lesbian.
+    // Siddharth Lohani - In first grade I had to go to the ER because I put a rock in my ear and I couldn't get it out.
 
     public IceBreaker () {
 
